5ed039bd73b9f8b50fa0b61ade6367c3557f81cd
kieran ayres <theayresyproject@gmail.com>
2014-10-23 19:58:48 +0100

    TileEntitys Glitch update

--- src/main/java/com/MysticalWonders/render/tile_entity/RenderMysticalChair.java
+++ src/main/java/com/MysticalWonders/render/tile_entity/RenderMysticalChair.java
@@ -24,12 +24,14 @@ public class RenderMysticalChair extends TileEntitySpecialRenderer{
 	
 	@Override
 	public void renderTileEntityAt(TileEntity tileentity, double x, double y, double z, float f) {
-		GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
-		GL11.glRotatef(180, 0F, 0F, 1F);
+
 		
 		this.bindTexture(texture);
 		
 		GL11.glPushMatrix();
+		
+		GL11.glTranslatef((float) x + 0.5F, (float) y + 1.5F, (float) z + 0.5F);
+		GL11.glRotatef(180, 0F, 0F, 1F);
 		
 		this.model.renderModel(0.0625F);
 		
